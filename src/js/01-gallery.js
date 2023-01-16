06cc55fc392d1e60dea6f12e1247a62d80500733
AleksandraDrzewiecka <111142458+AleksandraDrzewiecka@users.noreply.github.com>
2023-01-16 22:02:30 +0100

    html i js
    change

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,3 +2,36 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const list = document.querySelector(".gallery");
+
+galleryItems.forEach((image) => {
+    list.insertAdjacentHTML(
+        "beforeend",
+        `<div class="gallery__item">
+      <a class="gallery__link" href="${image.original}">
+        <img class="gallery__image" src="${image.preview}" data-source="${image.original}" alt="${image.description}" />
+      </a>
+    </div>`
+    );
+});
+
+list.addEventListener("click", imageModal);
+
+function imageModal(event) {
+    event.prevetDefault();
+    if (event.target.nodeName !== "IMG") {
+        return;
+    }else
+  {
+    const instance = basicLightbox.create(`
+    <img class="gallery__image" src="${event.target.dataset.source}" width="1280" height="720">
+    `);
+
+        instance.show();
+        
+}return;
+}
+
+
+
